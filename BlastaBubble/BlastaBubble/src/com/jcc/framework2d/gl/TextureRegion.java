ac27a2ac3000facc199641c6702d966e43bcaac1
Mouhammad Ali Al Jajeh <ali.jajeh@gmail.com>
2012-08-14 14:49:53 -0400

    Added Final float originalWidth, and originalHeight members
    Signed-off-by: Mouhammad Ali Al Jajeh <ali.jajeh@gmail.com>

--- BlastaBubble/BlastaBubble/src/com/jcc/framework2d/gl/TextureRegion.java
+++ BlastaBubble/BlastaBubble/src/com/jcc/framework2d/gl/TextureRegion.java
@@ -7,10 +7,14 @@ public class TextureRegion {
 	public final float u2, v2;
 	public final Texture texture;
 
+	public final float originalWidth, originalHeight;
+
 	public TextureRegion(Texture texture, float x, float y, float width,
 			float height) {
 		this.u1 = x / texture.width;
 		this.v1 = y / texture.height;
+		this.originalWidth = width;
+		this.originalHeight = height;
 		this.u2 = this.u1 + width / texture.width;
 		this.v2 = this.v1 + height / texture.height;
 		this.texture = texture;
